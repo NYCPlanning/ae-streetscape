75bf0f1eb399a83886458b12335bc910a7a99d5a
Tyler Matteo <tylermatteo434@gmail.com>
2023-09-11 14:25:55 -0400

    Adds eslint-plugin-prettier, husky, and lint-staged

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
-    "prettier",
+    "plugin:prettier/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {

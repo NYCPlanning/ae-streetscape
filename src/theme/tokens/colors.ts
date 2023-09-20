06942b9beb563548d676a933e863fe6b5c05ffc1
Tyler Matteo <tylermatteo434@gmail.com>
2023-09-20 14:16:17 -0400

    Scaffold theme files and add color tokens. Update package.json scripts with chakra cli and concurrently for generating token typings

---
+++ src/theme/tokens/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+  transparent: "transparent",
+  current: "currentColor",
+  white: "#FFFFFF",
+  gray: {
+    50: "#F7FAFC",
+    100: "#EDF2F7",
+    200: "#E2E8F0",
+    300: "#CBD5E0",
+    400: "#A0AEC0",
+    500: "#718096",
+    600: "#4A5568",
+    700: "#2D3748",
+    800: "#1A202C",
+  },
+  teal: {
+    50: "#E6FFFA",
+    500: "#319795",
+    600: "#2C7A7B",
+    700: "#285E61",
+  },
+};
---
+++ src/theme/tokens/index.ts
@@ -0,0 +1,33 @@
+import { blur } from "./blur";
+import { borders } from "./borders";
+import { breakpoints } from "./breakpoints";
+import { colors } from "./colors";
+import { fontSizes } from "./font-sizes";
+import { fontWeights } from "./font-weights";
+import { fonts } from "./fonts";
+import { letterSpacings } from "./letter-spacing";
+import { lineHeights } from "./line-heights";
+import { radii } from "./radii";
+import { shadows } from "./shadows";
+import { sizes } from "./sizes";
+import { space } from "./space";
+import { transition } from "./transition";
+import { zIndices } from "./z-indices";
+
+export const tokens = {
+  blur,
+  borders,
+  breakpoints,
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  letterSpacings,
+  lineHeights,
+  radii,
+  shadows,
+  sizes,
+  space,
+  transition,
+  zIndices,
+};

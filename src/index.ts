bbb242d64e08e6ae08fb90331340f7caf33b613a
Tyler Matteo <tylermatteo434@gmail.com>
2023-09-24 10:34:10 -0400

    Reorganize folder structure, configure vite library mode

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { theme } from "./theme";
+import { Button } from "@chakra-ui/react";
+
+export { theme, Button };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      exclude: ["src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    }),
+  ],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "MyLib",
+      formats: ["es"],
+      // the proper extensions will be added
+      fileName: "my-lib",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        "react",
+        "react-dom",
+        "framer-motion",
+        "@emotion/react",
+        "@emotion/styled",
+        "@chakra-ui/react",
+      ],
+    },
+  },
 });

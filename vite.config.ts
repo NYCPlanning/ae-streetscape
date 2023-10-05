48fceb731ffac5ed4b196043e991107bfad547d4
Tyler Matteo <tylermatteo434@gmail.com>
2023-10-05 15:45:43 -0400

    Add root index.ts file and configure vite and npm scripts for package publishing. Rename packing to @nycplanning/streetscape

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { externalizeDeps } from "vite-plugin-externalize-deps";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      exclude: ["src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)"],
+      insertTypesEntry: true,
+    }),
+    externalizeDeps(),
+  ],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+  },
 });

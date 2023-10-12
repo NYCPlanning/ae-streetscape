29523f0b0d243a907d8ce3aeb6b0da95093e0d07
Tyler Matteo <tylermatteo434@gmail.com>
2023-10-12 11:38:12 -0400

    Export button from src, add @chakra-ui/layout dependency so that it isn't including in bundle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { theme } from "./theme";
-import { Button } from "@chakra-ui/react";
 import { StreetscapeProvider } from "./StreetscapeProvider";
 
 export * from "@chakra-ui/layout";
-export { theme, Button, StreetscapeProvider };
+export * from "./components/Button";
+export { theme, StreetscapeProvider };

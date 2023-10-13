ec1409056b9cb7490b44b2422a33a2d32f92374b
Bryan Marchena <bmarchena@planning.nyc.gov>
2023-10-13 08:51:17 -0400

    Improve references to tokens, address feedback

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -4,78 +4,70 @@ export const buttonTheme = defineStyleConfig({
   baseStyle: {
     backgroundColor: "primary.600",
     borderRadius: "base",
-    // token?
     color: "white",
     px: 6,
     py: 3,
   },
   sizes: {
     sm: {
-      height: 11,
+      fontSize: "sm",
     },
     md: {
-      height: 12,
+      fontSize: "md",
     },
     lg: {
-      height: 14,
+      fontSize: "lg",
     },
   },
   variants: {
     primary: {
-      // token?
+      // TODO: Refactor setting boxShadow tokens
       boxShadow: "0 1.5 1.5 0 rgba(35, 78, 82, 0.08)",
       _disabled: {
         backgroundColor: "primary.500",
         color: "white",
-        // token?
         opacity: 0.64,
       },
       _hover: {
         backgroundColor: "brand.50",
-        // token?
         border: "1px solid",
         borderColor: "brand.800",
-        // token?
         boxShadow: "0 1 1.5 0 rgba(217, 107, 39, 0.18)",
         color: "gray.700",
-        fontWeight: "500",
+        fontWeight: "medium",
       },
       _active: {
+        boxShadow: "0 .25 .25 0 rgba(0, 0, 0, 0.08)",
         color: "white",
-        fontWeight: "500",
+        fontWeight: "medium",
         textDecorationLine: "underline",
       },
     },
     secondary: {
       backgroundColor: "white",
-      // token?
       border: "2px solid",
       borderColor: "primary.600",
-      // token?
       boxShadow: "0 1.5 1.5 0 rgba(44, 122, 123, 0.04)",
-      color: "gray.70",
+      color: "gray.700",
       _disabled: {
         backgroundColor: "gray.50",
-        // token?
         border: "2px solid",
         borderColor: "primary.600",
         color: "gray.700",
-        // token?
         opacity: 0.64,
       },
       _hover: {
         backgroundColor: "white",
-        // token?
         border: "2px solid",
         borderColor: "brand.800",
-        // token?
         boxShadow: "0 1 1.5 0 rgba(217, 107, 39, 0.18)",
         color: "gray.700",
-        fontWeight: "500",
+        fontWeight: "medium",
       },
       _active: {
+        boxShadow: "0 .25 .25 0 rgba(0, 0, 0, 0.08)",
         color: "gray.700",
-        fontWeight: "500",
+        fontWeight: "medium",
         textDecorationLine: "underline",
       },
     },

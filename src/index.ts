cd517806fb0fc64f19979e68e6a03f1fc57cf7d2
Tyler Matteo <tylermatteo434@gmail.com>
2023-10-13 17:06:34 -0400

    Export Chakra components and hooks from Chakra media query package

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { theme } from "./theme";
 import { StreetscapeProvider } from "./StreetscapeProvider";
 
 export * from "@chakra-ui/layout";
+export * from "@chakra-ui/media-query";
 export * from "./components";
 export { theme, StreetscapeProvider };

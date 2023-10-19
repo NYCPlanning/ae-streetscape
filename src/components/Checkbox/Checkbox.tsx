46130f42cb5606dc59190d60ff4d6a12bb434994
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2023-10-19 09:38:09 -0400

    Checkbox added, ready to be styled

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,5 @@
+import { Checkbox as ChakraCheckbox } from "@chakra-ui/react";
+import type { CheckboxProps as ChakraCheckboxProps } from "@chakra-ui/react";
+
+export const Checkbox = ChakraCheckbox;
+export interface CheckboxProps extends ChakraCheckboxProps {}
--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,5 +1,7 @@
 import { buttonTheme } from "./button";
+import { checkboxTheme } from "./checkbox";
 
 export const components = {
   Button: buttonTheme,
+  Checkbox: checkboxTheme,
 };

159e057f30c92fa3ff10567f80a5cac549fb7c47
Tyler Matteo <tylermatteo434@gmail.com>
2023-10-27 17:10:17 -0400

    Add decorator to Input stories, stub out Input theme using multi part style config API

--- src/theme/components/input.ts
+++ src/theme/components/input.ts
@@ -1,38 +1,51 @@
-import { defineStyleConfig } from "@chakra-ui/react";
+// import { defineStyleConfig } from "@chakra-ui/react";
+import { inputAnatomy as parts } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/styled-system";
 
-export const inputTheme = defineStyleConfig({
-  baseStyle: {
-    field: {
-      background: "black",
-      border: "1px solid",
-      borderColor: "gray.400",
-      borderRadius: "base",
-      color: "gray.600",
-      _hover: {
-        borderColor: "brand.800",
-      },
-      // TOKEN
-      padding: "8px 12px",
-    },
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(parts.keys);
+
+const baseStyle = definePartsStyle({
+  addon: {
+    borderRadius: "base",
+    px: 3,
+    py: 2,
   },
-  sizes: {
-    sm: {
-      field: {
-        height: 8,
-      },
-    },
-    md: {
-      field: {
-        height: 10,
-      },
-    },
-    lg: {
-      field: {
-        height: 12,
-      },
-    },
+  field: {
+    // Set default width to full (100%) so input takes width of parent. Chakra default theme does this as well.
+    width: "full",
+    borderRadius: "base",
+    px: 3,
+    border: "1px solid",
+    borderColor: "gray.400",
+    // TODO - fill in pseudo class styles below
+    _hover: {},
+    _invalid: {},
+    _focusVisible: {},
+    _disabled: {},
   },
+});
+
+const sizes = {
+  lg: definePartsStyle({
+    field: { fontSize: "md", height: 12 },
+  }),
+  md: definePartsStyle({
+    field: { fontSize: "sm", height: 10 },
+  }),
+  sm: definePartsStyle({
+    field: { fontSize: "sm", height: 8 },
+  }),
+};
+
+export const inputTheme = defineMultiStyleConfig({
+  baseStyle,
+  sizes,
   variants: {
     base: {},
+  },
+  defaultProps: {
+    size: "md",
+    variant: "base",
   },
 });

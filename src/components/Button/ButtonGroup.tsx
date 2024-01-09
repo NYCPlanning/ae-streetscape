349e5186676ae518099adc92c77069c4870d97b6
Tyler Matteo <tylermatteo434@gmail.com>
2024-01-09 13:16:56 -0500

    Passthrough IconButton and ButtonGroup components from Chakra and update Button theme styling for them

---
+++ src/components/Button/ButtonGroup.tsx
@@ -0,0 +1,5 @@
+import { ButtonGroup as ChakraButtonGroup } from "@chakra-ui/react";
+import type { ButtonGroupProps as ChakraButtonGroupProps } from "@chakra-ui/react";
+
+export const ButtonGroup = ChakraButtonGroup;
+export interface ButtonGroupProps extends ChakraButtonGroupProps {}
---
+++ src/components/Button/IconButton.tsx
@@ -0,0 +1,5 @@
+import { IconButton as ChakraIconButton } from "@chakra-ui/react";
+import type { IconButtonProps as ChakraIconButtonProps } from "@chakra-ui/react";
+
+export const IconButton = ChakraIconButton;
+export interface IconButtonProps extends ChakraIconButtonProps {}

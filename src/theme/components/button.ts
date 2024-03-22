d44aaf8cea99e0ee1432fb751baf32310228a33d
Tyler Matteo <tylermatteo434@gmail.com>
2024-03-22 15:45:49 -0400

    fix(button): Change button color to brand 900

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -26,7 +26,7 @@ export const buttonTheme = defineStyleConfig({
   },
   variants: {
     primary: {
-      backgroundColor: "primary.600",
+      backgroundColor: "brand.900",
       color: "white",
       // TODO: Refactor setting boxShadow tokens
       boxShadow: "0 1.5 1.5 0 rgba(35, 78, 82, 0.08)",

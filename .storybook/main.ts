db23f505cdc2b6fa24e7e17bfa6973289cacb400
jessicashanshanhuang <jessicashanshanhuangdesign@gmail.com>
2024-07-15 14:39:33 -0700

    feat(designs-plugin): Install packages and added designs add-on in main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-designs"
   ],
   framework: {
     name: "@storybook/react-vite",

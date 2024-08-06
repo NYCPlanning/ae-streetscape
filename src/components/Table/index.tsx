487c73247f02350cacc07f64ff22f2d762c3b548
tangoyankee <miller.tim108@gmail.com>
2024-08-06 13:42:21 -0400

    rexport from table index

---
+++ src/components/Table/index.tsx
@@ -0,0 +1,21 @@
+export {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "./Table";
+
+export type {
+  TableProps,
+  TableHeadProps,
+  TableBodyProps,
+  TableFooterProps,
+  TableRowProps,
+  TableCellProps,
+  TableCaptionProps,
+  TableContainerProps,
+} from "./Table";

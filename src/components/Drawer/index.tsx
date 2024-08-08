b8907bcd9bb63895ee0d49551120110403437e54
Tyler Matteo <tylermatteo434@gmail.com>
2024-08-08 12:10:04 -0400

    fix(Drawer): Add missing exports to Drawer barrel file

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,2 +1,10 @@
-export { Drawer } from "./Drawer";
-export type { DrawerProps } from "./Drawer";
+export {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "./Drawer";
+export type { DrawerProps, DrawerContentProps } from "./Drawer";

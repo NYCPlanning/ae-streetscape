e7a33361f8319e32560a07feb4d9e58c64e1967c
David Hochbaum <dhochbaum@planning.nyc.gov>
2025-02-20 18:13:53 -0500

    Buttons no longer change width on hover

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,17 @@
 import { Button as ChakraButton } from "@chakra-ui/react";
 import type { ButtonProps as ChakraButtonProps } from "@chakra-ui/react";
+import * as React from "react";
 
-export const Button = ChakraButton;
+export const Button = React.forwardRef(function Button(
+  props: ChakraButtonProps,
+  ref: React.ForwardedRef<unknown>,
+) {
+  const { children, ...rest } = props;
+  return (
+    <ChakraButton data-label={children} ref={ref} {...rest}>
+      {children}
+    </ChakraButton>
+  );
+});
+
 export interface ButtonProps extends ChakraButtonProps {}

ba0173ed0bc40d7b47a71999c03198b065e43d0a
David Hochbaum <dhochbaum@planning.nyc.gov>
2025-05-27 12:23:00 -0400

    Added isCancellable to Select

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -4,6 +4,7 @@ import { getThemingArgTypes } from "@chakra-ui/storybook-addon";
 import { Select } from "./Select";
 import { theme } from "../../theme";
 import { userEvent, within } from "@storybook/test";
+import { useArgs } from "@storybook/preview-api";
 
 const meta = {
   title: "Components/Select",
@@ -12,6 +13,7 @@ const meta = {
   argTypes: {
     ...getThemingArgTypes(theme, "Select"),
     placeholder: { type: "string" },
+    isCancellable: { table: { defaultValue: { summary: false } } },
   },
   decorators: [
     // Because <Select> defaults to 100% width, wrap stories in div with max width.
@@ -51,5 +53,52 @@ export const Disabled: Story = {
     size: "md",
     variant: "base",
     isDisabled: true,
+  },
+};
+
+const children = [
+  <option value={"Option 1"} key={"Option 1"}>
+    This is a long option name to make sure that the ellipsis is working. This
+    is a long option name to make sure that the ellipsis is working.
+  </option>,
+  <option value={"Option 2"} key={"Option 2"}>
+    This is also a long option name to make sure that the ellipsis is working.
+    This is also a long option name to make sure that the ellipsis is working.
+  </option>,
+];
+
+export const Cancellable: Story = {
+  args: {
+    isCancellable: true,
+    placeholder:
+      "This component has extra space on the right for the inclusion of the cancel button, but only when a selection has been made. You should pass down the additional handleCancel property.",
+    size: "md",
+    variant: "base",
+    textOverflow: "ellipsis",
+    children,
+  },
+  render: function Render(args) {
+    const [{ value }, updateArgs] = useArgs();
+
+    function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+      updateArgs({ value: event.target.value });
+    }
+
+    return (
+      <Select
+        {...args}
+        value={value}
+        onChange={(e) => handleChange(e)}
+        handleCancel={() => updateArgs({ value: "" })}
+      >
+        {args.children}
+      </Select>
+    );
+  },
+  // Test for user click
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByText(/This component has/));
   },
 };
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,5 +1,81 @@
 import { Select as ChakraSelect } from "@chakra-ui/react";
 import type { SelectProps as ChakraSelectProps } from "@chakra-ui/react";
+import { IconButton } from "../Button";
+import { CloseIcon } from "@chakra-ui/icons";
+import { FormControl } from "../FormControl";
 
-export const Select = ChakraSelect;
-export interface SelectProps extends ChakraSelectProps {}
+export interface SelectProps extends ChakraSelectProps {
+  isCancellable?: boolean;
+  handleCancel?: () => void;
+}
+
+export function Select(props: SelectProps) {
+  const {
+    children,
+    isCancellable = false,
+    value = undefined,
+    handleCancel = () => null,
+    ...rest
+  } = props;
+
+  if (!isCancellable) {
+    return (
+      <ChakraSelect {...rest} value={value}>
+        {children}
+      </ChakraSelect>
+    );
+  }
+
+  return (
+    <FormControl>
+      <ChakraSelect
+        {...rest}
+        variant={value ? "withRightPadding" : "base"}
+        value={value}
+      >
+        {children}
+      </ChakraSelect>
+      {value && (
+        <IconButton
+          aria-label={`Clear`}
+          _hover={{
+            border: 0,
+            svg: {
+              backgroundColor: "gray.200",
+            },
+          }}
+          _focus={{
+            border: 0,
+            svg: {
+              backgroundColor: "gray.200",
+            },
+          }}
+          _active={{
+            border: 0,
+          }}
+          pos={"absolute"}
+          minH={"unset"}
+          minW={"unset"}
+          height={"min-content"}
+          width={"min-content"}
+          bottom={2}
+          right={10}
+          cursor={"pointer"}
+          bg={"transparent"}
+          color={"inherit"}
+          onClick={() => {
+            handleCancel();
+          }}
+          icon={
+            <CloseIcon
+              boxSize={3}
+              p={0.5}
+              border={"1px solid"}
+              borderRadius={16}
+            />
+          }
+        />
+      )}
+    </FormControl>
+  );
+}

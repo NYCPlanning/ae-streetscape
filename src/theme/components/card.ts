dc06382aa9c886c0b8c8cdeddde64e76cd47e3d0
tangoyankee <miller.tim108@gmail.com>
2025-10-29 10:52:48 -0400

    style(card): Calm style
    Add a style definition for cards

--- src/theme/components/card.ts
+++ src/theme/components/card.ts
@@ -1,1 +1,22 @@
-export { cardTheme } from "@chakra-ui/theme/components/card";
+import { cardTheme as chakraCardTheme } from "@chakra-ui/theme/components/card";
+import { cardAnatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(cardAnatomy.keys);
+
+const variants = {
+  ...chakraCardTheme?.variants,
+  calm: definePartsStyle({
+    container: {
+      padding: "0.75rem",
+      borderRadius: "0.5rem",
+      backgroundColor: "gray.50",
+    },
+  }),
+};
+
+export const cardTheme = defineMultiStyleConfig({
+  ...chakraCardTheme,
+  variants,
+});

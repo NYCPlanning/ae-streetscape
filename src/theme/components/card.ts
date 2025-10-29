69fefd2e09846eb2d53dacadd8516a16e7160be3
tangoyankee <miller.tim108@gmail.com>
2025-10-29 10:52:48 -0400

    style(card): Calm style
    Add a style definition for cards

--- src/theme/components/card.ts
+++ src/theme/components/card.ts
@@ -1,1 +1,24 @@
-export { cardTheme } from "@chakra-ui/theme/components/card";
+import { cardTheme as chakraCardTheme } from "@chakra-ui/theme/components/card";
+import { cardAnatomy } from "@chakra-ui/anatomy";
+import { createMultiStyleConfigHelpers } from "@chakra-ui/react";
+
+const { definePartsStyle, defineMultiStyleConfig } =
+  createMultiStyleConfigHelpers(cardAnatomy.keys);
+
+// define custom styles for funky variant
+const variants = {
+  ...chakraCardTheme?.variants,
+  calm: definePartsStyle({
+    container: {
+      padding: "0.75rem",
+      borderRadius: "0.5rem",
+      backgroundColor: "gray.50",
+    },
+  }),
+};
+
+// export variants in the component theme
+export const cardTheme = defineMultiStyleConfig({
+  ...chakraCardTheme,
+  variants,
+});

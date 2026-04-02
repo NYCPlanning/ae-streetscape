8969bb971915331f9d680166d88612ba4ec8405e
David Hochbaum <dhochbaum@planning.nyc.gov>
2026-04-02 10:22:51 -0400

    Fixed combobox exports

--- src/components/Combobox/index.ts
+++ src/components/Combobox/index.ts
@@ -1,5 +1,14 @@
-export { Combobox, useListCollection } from "./Combobox";
+export {
+  Combobox,
+  useListCollection,
+  createListCollection,
+  useCombobox,
+} from "./Combobox";
 export type {
   ComboboxCollectionItemProps,
   ComboboxListCollectionProps,
+  ComboboxInputValueChangeDetails,
+  ListCollection,
+  ComboboxSelectionDetails,
+  UseComboboxReturn,
 } from "./Combobox";
